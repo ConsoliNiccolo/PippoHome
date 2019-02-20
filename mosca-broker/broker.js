ef78b6140e56b23385715ea006238fde8269c1ae
ConsoliNiccolo <niccoloconsoli@gmail.com>
2019-02-20 22:01:46 +0100

    321

--- mosca-broker/broker.js
+++ mosca-broker/broker.js
@@ -3,21 +3,27 @@
 // ######################################################
 var mosca = require('mosca');
 
-// var ascoltatore = {
-//     //using ascoltatore
-//     type: 'mongo',
-//     url: 'mongodb+srv://Niccos:Reitalia88@cluster0-9rqfj.mongodb.net/mqtt?retryWrites=true',
-//     pubsubCollection: 'ascoltatori',
-//     mongo: {}
-//   };
+var ascoltatore = {
+    //using ascoltatore
+    type: 'mongo',
+    url: 'mongodb+srv://Niccos:Reitalia88@cluster0-9rqfj.mongodb.net/mqtt?retryWrites=true',
+    pubsubCollection: 'ascoltatori',
+    mongo: {}
+  };
   
-//   var settings = {
-//     port: 1883,
-//     backend: ascoltatore
-//   };
+  var settings = {
+    port: 1883,
+    backend: ascoltatore
+  };
 // ######################################################
 //                 Not required for db
 // ######################################################
+
+  // var db = new mosca.persistence.Mongo({
+  //   url: 'mongodb://localhost:27017/mqtt'
+  // }
+  // );
+  // db.wire(server);
 
 
 // ######################################################
@@ -26,12 +32,6 @@ var mosca = require('mosca');
  
   var server = new mosca.Server(settings);
   server.on('ready', setup); 
-  var db = new mosca.persistence.Mongo({
-    url: 'mongodb+srv://Niccos:Reitalia88@cluster0-9rqfj.mongodb.net/mqtt?retryWrites=true'
-  }
-  );
-  db.wire(server);
-
 
   // fired when the mqtt server is ready
   function setup() {
